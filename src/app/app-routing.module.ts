e3b6dc96e59fd5e72442c91a394aa58323160305
drest <atiroandres@gmail.com>
2023-07-20 18:57:17 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SimpsonComponent } from './pages/simpson/simpson.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:SimpsonComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
